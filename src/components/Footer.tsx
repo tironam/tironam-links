efe6cdd18881be65c338c721706fc93394ff8e2d
tironam <tironamichael@gmail.com>
2022-06-27 17:19:40 -0400

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 const Footer = styled.div`
 	color: #fff;
-	position: absolute center;
 	margin-top: 60px;
+	position: absolute center;
+	bottom: 0;
 	text-align: center;
 	font-size: 12px;
 	font-family: 'Alata', sans-serif;
