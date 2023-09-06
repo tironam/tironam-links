67d9606a30e7cb59526a9afeffd053492dd4b794
tironam <tironamichael@gmail.com>
2023-09-05 21:15:25 -0400

    Updated media-query height for button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,19 @@
-import { ButtonWrap } from './Button.styles';
+import { ButtonWrap } from './Button.styles'
 
 type Props = {
-	children: string;
-	link: string;
-	newTab?: string;
-};
+  children: string
+  link: string
+  newTab?: string
+}
 
 const Button: React.FC<Props> = ({ link, children, newTab }) => (
-	<ButtonWrap href={link} target={newTab}>
-		{children}
-	</ButtonWrap>
-);
+  <ButtonWrap href={link} target={newTab}>
+    {children}
+  </ButtonWrap>
+)
 
-export default Button;
+export default Button
 
 Button.defaultProps = {
-	newTab: '_blank',
-};
+  newTab: '_blank',
+}
