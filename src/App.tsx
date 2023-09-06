f1add3e332cdce42c7225bc30cee1b1db5168967
tironam <tironamichael@gmail.com>
2023-09-05 20:55:47 -0400

    Added fanhome link

--- src/App.tsx
+++ src/App.tsx
@@ -20,44 +20,41 @@ const App = () => {
 	let year = new Date().getFullYear();
 
 	return (
-		<>
-			<div className='wrapper'>
-				<Header>@tironam</Header>
-				<ProfilePic img={picture} alt='Michael Tirona picture' />
-				<Title>Michael Tirona</Title>
-				<IconSectionWrap>
-					<IconBtn
-						link='https://www.tiktok.com/@tironam'
-						icon={<FaTiktok />}
-					/>
-					<IconBtn
-						link='https://www.instagram.com/tironam'
-						icon={<FaInstagram />}
-					/>
-					<IconBtn
-						link='mailto:tironamichael@gmail.com'
-						icon={<AiOutlineMail />}
-					/>
-				</IconSectionWrap>
-				<Text>Content creator, web developer, and artist</Text>
-				<BtnWrapper>
-					<Button link='https://tironam.com'>
-						Web Development Portfolio
-					</Button>
-					<Button link='https://mcuandyou.com'>
-						My Marvel Resource site
-					</Button>
-				</BtnWrapper>
-			</div>
+    <>
+      <div className='wrapper'>
+        <Header>@tironam</Header>
+        <ProfilePic img={picture} alt='Michael Tirona picture' />
+        <Title>Michael Tirona</Title>
+        <IconSectionWrap>
+          <IconBtn link='https://www.tiktok.com/@tironam' icon={<FaTiktok />} />
+          <IconBtn
+            link='https://www.instagram.com/tironam'
+            icon={<FaInstagram />}
+          />
+          <IconBtn
+            link='mailto:tironamichael@gmail.com'
+            icon={<AiOutlineMail />}
+          />
+        </IconSectionWrap>
+        <Text>Content creator, web developer, and artist</Text>
+        <BtnWrapper>
+          <Button link='https://bit.ly/3iKUN1F'>
+            'Get your own Ecto-1 1:8 scale model from Fanhome and use promo code
+            BUSTERSGHOST'
+          </Button>
+          <Button link='https://tironam.com'>Web Development Portfolio</Button>
+          <Button link='https://mcuandyou.com'>My Marvel Resource site</Button>
+        </BtnWrapper>
+      </div>
 
-			<Fwrapper>
-				<Footer>
-					Michael Tirona {year} | Built with TypeScript, React,
-					Styled-Components, and React-Icons
-				</Footer>
-			</Fwrapper>
-		</>
-	);
+      <Fwrapper>
+        <Footer>
+          Michael Tirona {year} | Built with TypeScript, React,
+          Styled-Components, and React-Icons
+        </Footer>
+      </Fwrapper>
+    </>
+  )
 };
 
 export default App;
