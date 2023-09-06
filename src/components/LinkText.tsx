b8cec998211e25402db7c41e8baed74e44dec048
tironam <tironamichael@gmail.com>
2023-09-05 21:08:16 -0400

    Added Fanhome link

---
+++ src/components/LinkText.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const LinkText = styled.p`
+  font-size: 18px;
+  font-family: 'Alata', sans-serif;
+  text-align: center;
+  color: #fff;
+  margin: 10px 12px 10px 12px;
+  @media (max-width: 480px) {
+    font-size: 16px;
+    margin: 15px 6px 10px 15px;
+  }
+`
+
+export default LinkText
